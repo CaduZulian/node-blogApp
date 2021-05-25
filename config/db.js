040725120495eca0944c617252f036098c435661
Your Name <you@example.com>
2021-05-25 18:27:35 -0300

    initial commit

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == "production"){
-  module.exports = {mongoURI: "mongodb+srv://dbUser:19bili09@blogapp-prod.yt0hh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"}
+  module.exports = {mongoURI: "mongodb+srv://dbUser:19bili09@blogapp-prod.yt0hh.mongodb.net/BlogApp-prod?retryWrites=true&w=majority"}
 } else {
   module.exports = {mongoURI: "mongodb://localhost/blogapp"}
 }
