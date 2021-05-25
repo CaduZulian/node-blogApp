6f187bf35f072bd284bcb42f919e69875e18c16c
Your Name <you@example.com>
2021-05-25 19:16:19 -0300

    initial commit

--- app.js
+++ app.js
@@ -54,13 +54,13 @@
 
 //Rotas
 
-  app.get("/404", (req, res) => {
+  app.get("/", (req, res) => {
     Postagem.find().populate("categoria").lean().sort({data: "desc"}).then((postagens) => {
       res.render("index", {postagens: postagens})
     }).catch((err) => {
       console.log()
       req.flash("error_msg", "Houve um erro interno")
-      res.redirect("/")
+      res.redirect("/404")
     })
   })
 
@@ -78,9 +78,8 @@ app.get("/postagem/:slug" , (req, res) => {
   })
 })
 
-  app.get("/", (req, res) => {
-    //res.send("Erro 404!")
-    res.render("index")
+  app.get("/404", (req, res) => {
+    res.send("Erro 404!")
   })
 
   app.get("/categorias", (req, res) => {
